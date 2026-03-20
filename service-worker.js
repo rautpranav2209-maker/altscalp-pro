074bd63b2c56e6221db0bb3d2844f43ed2a95c38
Pranav Raut <rautpranav2209@gmail.com>
2026-03-21 00:53:57 +0530

    Fix: Bounced Service Worker cache (v1 -> v2) to force UI update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'altscalp-v1';
+const CACHE_NAME = 'altscalp-v2';
 const ASSETS = [
   '/',
   '/index.html',
@@ -6,8 +6,19 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
